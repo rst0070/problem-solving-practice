12e510dd4b8bb2c6dcb8b042b6eb6448b4195b08
rst0070 <52444137+rst0070@users.noreply.github.com>
2020-11-07 11:22:04 +0900

    Create 별_찍기_-_10.cpp

---
+++ alim_baekjoon/Recursion/별_찍기_-_10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+void printMid(int n, int size){
+    
+}
+void print(int n, int size, int x, int y){
+    if(size > 3){
+        print(n, size/3);
+        printMid(n, size/3);
+        print(n, size/3);
+        return;
+    }
+    
+    
+}
+int main()
+{
+    return 0;
+}
