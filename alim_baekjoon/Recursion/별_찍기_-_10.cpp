c1f842b2fb6e97d501426ac500f50ed2466bca04
rst0070 <52444137+rst0070@users.noreply.github.com>
2020-11-07 13:46:17 +0900

    Update 별_찍기_-_10.cpp
    채점 완료

--- alim_baekjoon/Recursion/별_찍기_-_10.cpp
+++ alim_baekjoon/Recursion/별_찍기_-_10.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
+char data[6561][6561];
 
-using namespace std;
-void printMid(int n, int size){
-    
-}
-void print(int n, int size, int x, int y){
+/*
+재귀 방식으로 data에 프린트할 내용을 저장시킨다.
+*/
+void save(int size, int x, int y){
     if(size > 3){
-        print(n, size/3);
-        printMid(n, size/3);
-        print(n, size/3);
+        int nextSize = size/3;
+        save(nextSize, x, y);
+        save(nextSize, x + nextSize, y);
+        save(nextSize, x + 2*nextSize, y);
+        
+        save(nextSize, x, y + nextSize);
+        save(nextSize, x + 2*nextSize, y + nextSize);
+        
+        save(nextSize, x, y + 2*nextSize);
+        save(nextSize, x + nextSize, y + 2*nextSize);
+        save(nextSize, x + 2*nextSize, y + 2*nextSize);
         return;
     }
+    data[x][y] = '*';
+    data[x+1][y] = '*';
+    data[x+2][y] = '*';
     
+    data[x][y+1] = '*';
+    data[x+2][y+1] = '*';
+    
+    data[x][y+2] = '*';
+    data[x+1][y+2] = '*';
+    data[x+2][y+2] = '*';
     
 }
-int main()
-{
+
+int main(){
+    int n;
+    std::cin >> n;
+    save(n, 0, 0);
+    for(int i=0; i<n; i++){
+        for(int j=0; j < n; j++){
+            if(data[j][i]) std::cout << data[j][i];
+            else std::cout << ' ';
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
+
