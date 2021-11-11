a58ddc1e830ef01e3ec682495dd2012ab2e01b60
Wonbin Kim <52444137+rst0070@users.noreply.github.com>
2021-11-11 22:37:29 +0900

    Update 텀_프로젝트.java

--- alim_baekjoon/DFS and BackTracking/텀_프로젝트.java
+++ alim_baekjoon/DFS and BackTracking/텀_프로젝트.java
@@ -5,16 +5,17 @@ class Main{
 
     static int n;
     static int[] graph;
-    static boolean[] checked;
+    static int[] checked;// 0:탐색하지않음, 1: 사이클 없음, 2: 사이클 있음
     static boolean[] visited;
     static boolean[] inCycle;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int testCase = Integer.parseInt(br.readLine());
         while(testCase-- > 0){
             n = Integer.parseInt(br.readLine());
             graph = new int[n+1];
-            checked = new boolean[n+1];
+            checked = new int[n+1];
             visited = new boolean[n+1];
             inCycle = new boolean[n+1];
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,36 +23,66 @@ class Main{
                 graph[i] = Integer.parseInt(st.nextToken());
             }
 
+            for(int i = 1; i <= n; i++){
+                if(checked[i] == 0) findCycle(i);
+            }
 
+            int count = 0;
+            for(int i = 1; i <= n; i++){
+                if(checked[i] == 1) count++;
+            }
+            bw.write(count+"\n");
         }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
     /**
      * 겹침이 발생한 지점을 반환한다.
      * 이 함수는 시작할때 visited[now] = true;
      * 끝날때 visited[now] = false;를 한다.
-     * 재귀를 통해서 사이클이 발생하는 지점을 알 수 있을때
-     * 이 지점을 visited로 조회해보면 자신보다 먼저 방문한곳이면 true,
-     * 나중에 방문한 곳 이면 false가 된다.
-     * 이를 통해서 자신이 사이클에 속한것 인지 알 수 있다.
+     * 재귀를 통해서 사이클이 발생하는 지점을 알 수 있을것이다. 이를 점 p로 하면면
+     * visited[p]의 의미가 중요하다는것을 알게된다.
+     * visited[p] = true: p는 now보다 먼저 방문했다.
+     * visited[p] = false: p는 now보다 나중에 방문되었다.
+     * 왜냐하면 now의 다음점들을 방문하는 함수는 이미 작동이 끝났기때문.
      * 
-     * 사이클이 겹친경우도 있을 수 있다..
-     * 하지만 점 p를 방문했다면 p의 모든 가능한 사이클을 탐색했다는것.
-     * 따라서 p를 두번 탐색할 필요는 없다.
+     * 이를 통해서 사이클의 시작점이
+     * now 혹은 now보다 먼저 방문된 점이라면,
+     * now는 사이클에 속한것이다.
      * 
-     * 어떤 점 q의 사이클 가능성을 확인할 때,
-     * 점 p가 q와 연결돼있고 이미 탐색한 경우일때, 
-     * 1. p가 사이클에 속해 있다면, p는 다른점 하나만 가리킬 수 있으므로
-     * 새로운 경로를 탐색하지 못하며, q는 이 사이클에 속하지 못한것이다.
-     * 따라서 p를 새로 탐색할 필요가 없다.
-     * 2. p가 사이클에 속해 있지 않다면, 1과같이 p는 다른점 하나만 가리킬 수 있으므로
-     * 새로운 경로를 탐색하지 못한다. 즉 p는 사이클에 속하지 않았다는것이 확정된것.
-     * 또한 q를 탐색하지 않은경우라면 p를 탐색했을때 q가 나오지 않는다는것이므로
-     * p를 탐색할 필요가 없다.
+     * findCycle(p)를 진행하는 중에 p가 q를 가리킨다.
+     * 이것의 의미는 p가 사이클에 속하기위해선 p와 q모두 사이클에 속해야한다는것.
+     * 이는 이 문제의 조건을 변경하여 p가 여러개의 점을 가리켜도 성립한다.
+     * (p가 a, b, c를 가릴킬때 각각 p, a || p, b || p, c 를 포함하는 사이클의 존재 여부를 찾는것)
      * 
-     * 따라서 한번 탐색한 지점은 탐색할 필요가 없다.
+     * 이때 이미 findCycle(q)가 실행된 상태라면?
+     * q가 어떤 사이클에 속해있거나 사이클에 속하지 않아도
+     * p는 그 사이클에 존재하지 않는다는 의미이다.
+     * 따라서 q를 탐색할 필요가 없다.
+     * 
+     * 따라서 findCycle(q)를 실행했다면 q를 탐색했다는 것을 기록해야한다.
+     * 이를 checked[q]에 기록하였다.
+     * 
      */
     static int findCycle(int now){
-        if()
+        visited[now] = true;
+        int next = graph[now];
+        int result = 0;
+        if(checked[next] == 0){
+            if(visited[next]){
+                checked[now] = 2;
+                result = next;
+            }else{
+                result = findCycle(next);
+                if( result != 0 && visited[result]) checked[now] = 2;
+                else checked[now] = 1;
+            }
+        }else{
+            checked[now] = 1;
+        }
+        visited[now] = false;
+        return result;
     }
 }
