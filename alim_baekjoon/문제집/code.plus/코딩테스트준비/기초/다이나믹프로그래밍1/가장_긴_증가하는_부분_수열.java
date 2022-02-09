e7d6728e5358b5f8960a9fb2b820d6d1a0a24208
rst0070 <52444137+rst0070@users.noreply.github.com>
2022-02-09 10:56:35 +0900

    가장 긴 증가하는 부분 수열

--- alim_baekjoon/문제집/code.plus/코딩테스트준비/기초/다이나믹프로그래밍1/가장_긴_증가하는_부분_수열.java
+++ alim_baekjoon/문제집/code.plus/코딩테스트준비/기초/다이나믹프로그래밍1/가장_긴_증가하는_부분_수열.java
@@ -3,6 +3,9 @@ import java.util.*;
 
 public class 가장_긴_증가하는_부분_수열 {
 
+    /**
+     * 재귀함수 startFromP가 p에서 시작하지 않는 경우를 계산하는 실수함
+     */
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,31 +15,27 @@ public class 가장_긴_증가하는_부분_수열 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= N; i++) A[i] = Integer.parseInt(st.nextToken());
 
-        startFromP(0, 1);
-        System.out.println(memo[1]);
+        
+        int result = 0;
+        for(int i = 1; i <= N; i++)
+            if(result < startFromP(i)) result = startFromP(i);
+        System.out.println(result);
     }
 
     static int N;
     static int[] A = new int[1001];
     static int[] memo = new int[1001];
-    static boolean[] checked = new boolean[1001];
 
-    /**
-     * 
-     * 어느부분이 문제..?
-     * 반례
-     * 6
-     * 1 5 6 2 3 4
-     */
 
-    static int startFromP(int prev, int p){
+    //A[p]를 포함하는 경우만
+    static int startFromP(int p){
         if(p > N) return 0;
-        if(prev >= A[p]) return startFromP(prev, p+1);
-        if(checked[p]) return memo[p];
+        if(memo[p] > 0) return memo[p];
 
-        memo[p] = 1 + startFromP(A[p], p+1);
-        memo[p] = Math.max(memo[p], startFromP(prev, p+1));
-        checked[p] = true;
+        memo[p] = 1;
+        for(int i = p + 1; i <= N; i++){
+            if(A[p] < A[i]) memo[p] = Math.max(memo[p], 1 + startFromP(i));
+        }
 
         return memo[p];
     }
