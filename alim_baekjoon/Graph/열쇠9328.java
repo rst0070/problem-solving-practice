bc6d54b266199aafedf50e1f6b9976136a47c9f9
rst0070 <52444137+rst0070@users.noreply.github.com>
2022-07-18 09:10:18 +0900

    주석추가

--- alim_baekjoon/Graph/열쇠9328.java
+++ alim_baekjoon/Graph/열쇠9328.java
@@ -35,17 +35,21 @@ public class 열쇠9328 {
 
             int result = 0;
 
+            //탐색시작
             while(!toGo.isEmpty()){
                 Point now = toGo.poll();
                 char val = now.getVal();
                 
+                //벽이면 무시
                 if(val == '*') continue;
 
+                //못여는 문이면 못열었다고 저장
                 if('A' <= val && val <= 'Z' && !key[val - 'A']){
                     lockedDoors[val - 'A'].add(now);
                     continue;
                 }
 
+                //새로운 열쇠가 열수있는 못연문들을 다음에 방문
                 if('a' <= val && val <= 'z' && !key[val - 'a']){
                     key[val - 'a'] = true;
                     while(!lockedDoors[val-'a'].isEmpty()){
@@ -55,8 +59,10 @@ public class 열쇠9328 {
                     }
                 }
 
+                //문서
                 if(val == '$') result++;
 
+                //다음방문지점들
                 for(int i = 0; i < 4; i++){
                     Point next = new Point(now.h + dir[i][0], now.w + dir[i][1]);
                     if(next.h >= 0 && next.h < H && next.w >= 0 && next.w < W && !visit[next.h][next.w]){
